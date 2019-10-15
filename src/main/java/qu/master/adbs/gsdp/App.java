e1aeaeac1ecee5de2065254a573ddea21509dc23
Abdulrahman Al-hajry <aalhajry1991@hotmail.com>
2019-10-15 12:50:33 +0300

    Initial Commit

---
+++ src/main/java/qu/master/adbs/gsdp/App.java
@@ -0,0 +1,13 @@
+package qu.master.adbs.gsdp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
